ba2bf11a172998ac848ba23270036d41a0a75a4f
iheppe <iheppe@uw.edu>
2021-05-06 12:35:20 -0700

    added initial files

---
+++ iQuiz/iQuiz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iQuiz
+//
+//  Created by Isabella Heppe on 5/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iQuiz/iQuiz/iQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  iQuizApp.swift
+//  iQuiz
+//
+//  Created by Isabella Heppe on 5/6/21.
+//
+
+import SwiftUI
+
+@main
+struct iQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
